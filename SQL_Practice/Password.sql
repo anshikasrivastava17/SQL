78b87e6dd15b86ada4af9387b46ccfba3abbe4db
Anshika Srivastava <115107568+anshikasrivastava17@users.noreply.github.com>
2025-03-14 01:59:51 +0530

    generate password

---
+++ SQL_Practice/Password.sql
@@ -0,0 +1,5 @@
+/* "If a USERNAME is ‘JOHNDOE’ and his DEPARTMENT is ‘SALES’, how would you generate a password using the first 3 characters of the USERNAME and the first 3 characters of the DEPARTMENT. 
+The outcome should be ‘JOHSAL’." */
+
+SELECT CONCAT(SUBSTR('JOHNDOE',1,3), SUBSTR('SALES',1,3))  
+FROM DUAL;
